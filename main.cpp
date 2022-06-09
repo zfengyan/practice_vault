689870294e65b8db98f802e7d61760420083d75c
zmocheng <zmocheng@gmail.com>
2022-06-09 23:42:45 +0200

    Add generic traverse function.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "binarysearch.h"
 #include <iostream>
 
+/* container_type */
+typedef vector<int> container_type;
+
+/* example: a generic traversing function */
+std::ostream& operator<<(std::ostream& os, const container_type& c)
+{
+	for (container_type::const_iterator it = c.begin(); it != c.end(); ++it)
+		os << *it << " ";
+	return os;
+}
+
+
+
+
 int main()
 {
 	vector<int> sorted_array = { 1,2,3,4,5,6 };
+	std::cout << "input sorted array is\n";
+	std::cout << sorted_array << '\n';
 	int target = 1;
+	std::cout << "target value is: " << target << '\n';
 	BinarySearch<int> bs(sorted_array, target);
-	std::cout << bs.search() << '\n';
+	std::cout << "search result (return index if exists): " << bs.search() << '\n';
 	return 0;
 }
