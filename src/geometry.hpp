e0236205f22ea1f0baaf1af5f7948795f60ff622
SEUZFY <zmocheng@gmail.com>
2022-12-05 16:28:15 +0100

    Add code in Point class.

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 template<typename T>
 class Point
 {
 public:
 
-    // constructors
+    // constructors --------------------------------------------------------------------------------
     
     Point():_x(T(0)), _y(T(0))
     {} // default constructor
@@ -20,15 +21,17 @@ public:
         _x(static_cast<T>(px)), _y(static_cast<T>(py)) {}
 
 
-    // operators
+    // operators --------------------------------------------------------------------------------
 
     // operator+ : yields a new point
-    // a+b => a.operator+(b)
+    // a+b => a.operator+(b), const qualifier: allow const object to access this method
     Point<T> operator+(const Point<T>& rhs)const 
     {        
         return Point(this->x() + rhs.x(), this->y() + rhs.y());
     }
 
+    // operator+ : yields a new point
+    // two different types of points, const qualifier: allow const object to access this method
     template <typename U>
     Point<T> operator+(Point<U>& rhs)const
     {
@@ -37,9 +40,65 @@ public:
         return Point(this->x() + rhs_x, this->y() + rhs_y);
     }
 
+    // operator- : yields a new point
+    // a-b => a.operator-(b), const qualifier: allow const object to access this method
+    Point<T> operator-(const Point<T>& rhs)const
+    {
+        return Point(this->x() - rhs.x(), this->y() - rhs.y());
+    }
+
+    // operator+ : yields a new point
+    // two different types of points, const qualifier: allow const object to access this method
+    template <typename U>
+    Point<T> operator-(Point<U>& rhs)const
+    {
+        T rhs_x = static_cast<T>(rhs.x());
+        T rhs_y = static_cast<T>(rhs.y()); // conversion
+        return Point(this->x() - rhs_x, this->y() - rhs_y);
+    }
+
+    // operator += : yields a new point
+    Point<T>& operator+=(const Point<T>& rhs)
+    {
+        this->x() += rhs.x();
+        this->y() += rhs.y();
+        return *this;
+    }
+
+    // operator += : yields a new point
+    // two different types of points
+    template <typename U>
+    Point<T>& operator+=(const Point<U>& rhs)
+    {
+        T rhs_x = static_cast<T>(rhs.x());
+        T rhs_y = static_cast<T>(rhs.y()); // conversion
+        this->x() += rhs_x;
+        this->y() += rhs_y;
+        return *this;
+    }
+
+    // operator -= : yields a new point
+    Point<T>& operator-=(const Point<T>& rhs)
+    {
+        this->x() -= rhs.x();
+        this->y() -= rhs.y();
+        return *this;
+    }
+
+    // operator -= : yields a new point
+    // two different types of points
+    template <typename U>
+    Point<T>& operator-=(const Point<U>& rhs)
+    {
+        T rhs_x = static_cast<T>(rhs.x());
+        T rhs_y = static_cast<T>(rhs.y()); // conversion
+        this->x() -= rhs_x;
+        this->y() -= rhs_y;
+        return *this;
+    }
     
 
-    // methods
+    // methods --------------------------------------------------------------------------------
     
     // get the x coordinate
     T& x() { return _x; } // return reference, so that we can both access and modify the x coordinate
@@ -48,6 +107,42 @@ public:
     // get the y coordinate
     T& y() { return _y; } // return reference, so that we can both access and modify the x coordinate
     const T& y()const { return _y; } // const version, for const objects
+
+    // get the distance between the current point and the origin((0, 0) by default)
+    double distance()const
+    {
+        message("calculate the distance from the origin(0, 0)");
+        Point<T> p0; // origin - (0, 0) by default
+        return distance(p0);
+    }
+
+    // get the distance between the current point and the point rhs
+    double distance(const Point<T>& rhs)const
+    {
+        return std::sqrt((rhs.x() - this->x()) * (rhs.x() - this->x()) + (rhs.y() - this->y()) * (rhs.y() - this->y()));
+    }
+
+    // get the distance if two points are not the same type
+    template<typename U>
+    double distance(const Point<U>& rhs)const
+    {
+        T rhs_x = static_cast<T>(rhs.x());
+        T rhs_y = static_cast<T>(rhs.y()); // conversion
+        Point<T> p_conversion(rhs_x, rhs_y);
+        return distance(p_conversion);
+    }
+
+    // print a point, const qualifier: allow const object to access this method
+    void print()const
+    {
+        std::cout << "(" << this->x() << ", " << this->y() << ")" << '\n';
+    }
+
+    // print a message
+    void message(const char* msg)const
+    {
+        std::cout << "-- " << msg << '\n';
+    }
 
     // ostream overloading, for printing the info of the Point<T> objects
     friend std::ostream& operator<<(std::ostream& os, const Point& p)
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 int main()
 {
 	Point<int> p1(2.0, 2.0);
-	Point<double> p2(1.0, 1.0);
-	std::cout << (p1 + p2);
+	Point<double> p2(1.0, 2.0);
+	std::cout << p2.distance() << '\n';
+	std::cout << p2.distance(p1) << '\n';
 	return 0;
 }
