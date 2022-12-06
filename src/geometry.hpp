9013b5d47d1af715068c8de8d98287ec3a0c623e
SEUZFY <zmocheng@gmail.com>
2022-12-06 14:26:26 +0100

    Add code to perform rotate operation.

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -1,135 +1,114 @@
 #pragma once
 
-#include <iostream>
+#define _USE_MATH_DEFINES
 #include <cmath>
+#include <iostream>
+#include <initializer_list> // for delegate constructor
 
-template<typename T>
 class Point
 {
 public:
 
     // constructors --------------------------------------------------------------------------------
     
-    Point():_x(T(0)), _y(T(0))
+    Point():_x(0.0), _y(0.0)
     {} // default constructor
 
-    Point(T px, T py):_x(px), _y(py)
+    Point(double px, double py):_x(px), _y(py)
     {} // constructor with two parameters
 
     template <typename U> // if the constructor gets different @param types
     Point(U px, U py) :
-        _x(static_cast<T>(px)), _y(static_cast<T>(py)) {}
+        _x(static_cast<double>(px)), _y(static_cast<double>(py)) 
+    {
+        message("data type will be converted to double");
+    }
 
 
     // operators --------------------------------------------------------------------------------
 
     // operator+ : yields a new point
     // a+b => a.operator+(b), const qualifier: allow const object to access this method
-    Point<T> operator+(const Point<T>& rhs)const 
+    Point operator+(const Point& rhs)const 
     {        
         return Point(this->x() + rhs.x(), this->y() + rhs.y());
     }
 
-    // operator+ : yields a new point
-    // two different types of points, const qualifier: allow const object to access this method
-    template <typename U>
-    Point<T> operator+(Point<U>& rhs)const
-    {
-        T rhs_x = static_cast<T>(rhs.x());
-        T rhs_y = static_cast<T>(rhs.y()); // conversion
-        return Point(this->x() + rhs_x, this->y() + rhs_y);
-    }
-
     // operator- : yields a new point
     // a-b => a.operator-(b), const qualifier: allow const object to access this method
-    Point<T> operator-(const Point<T>& rhs)const
+    Point operator-(const Point& rhs)const
     {
         return Point(this->x() - rhs.x(), this->y() - rhs.y());
     }
 
-    // operator+ : yields a new point
-    // two different types of points, const qualifier: allow const object to access this method
-    template <typename U>
-    Point<T> operator-(Point<U>& rhs)const
-    {
-        T rhs_x = static_cast<T>(rhs.x());
-        T rhs_y = static_cast<T>(rhs.y()); // conversion
-        return Point(this->x() - rhs_x, this->y() - rhs_y);
-    }
-
     // operator += : yields a new point
-    Point<T>& operator+=(const Point<T>& rhs)
+    Point& operator+=(const Point& rhs)
     {
         this->x() += rhs.x();
         this->y() += rhs.y();
         return *this;
     }
 
-    // operator += : yields a new point
-    // two different types of points
-    template <typename U>
-    Point<T>& operator+=(const Point<U>& rhs)
-    {
-        T rhs_x = static_cast<T>(rhs.x());
-        T rhs_y = static_cast<T>(rhs.y()); // conversion
-        this->x() += rhs_x;
-        this->y() += rhs_y;
-        return *this;
-    }
-
     // operator -= : yields a new point
-    Point<T>& operator-=(const Point<T>& rhs)
+    Point& operator-=(const Point& rhs)
     {
         this->x() -= rhs.x();
         this->y() -= rhs.y();
         return *this;
     }
 
-    // operator -= : yields a new point
-    // two different types of points
-    template <typename U>
-    Point<T>& operator-=(const Point<U>& rhs)
-    {
-        T rhs_x = static_cast<T>(rhs.x());
-        T rhs_y = static_cast<T>(rhs.y()); // conversion
-        this->x() -= rhs_x;
-        this->y() -= rhs_y;
-        return *this;
-    }
-    
-
     // methods --------------------------------------------------------------------------------
     
     // get the x coordinate
-    T& x() { return _x; } // return reference, so that we can both access and modify the x coordinate
-    const T& x()const { return _x; } // const version, for const objects
+    double& x() { return _x; } // return reference, so that we can both access and modify the x coordinate
+    const double& x()const { return _x; } // const version, for const objects
 
     // get the y coordinate
-    T& y() { return _y; } // return reference, so that we can both access and modify the x coordinate
-    const T& y()const { return _y; } // const version, for const objects
+    double& y() { return _y; } // return reference, so that we can both access and modify the x coordinate
+    const double& y()const { return _y; } // const version, for const objects
 
     // get the distance between the current point and the origin((0, 0) by default)
     double distance()const
     {
         message("calculate the distance from the origin(0, 0)");
-        Point<T> p0; // origin - (0, 0) by default
+        Point p0; // origin - (0, 0) by default
         return distance(p0);
     }
 
     // get the distance between the current point and the point rhs
-    double distance(const Point<T>& rhs)const
+    double distance(const Point& rhs)const
     {
         return std::sqrt((rhs.x() - this->x()) * (rhs.x() - this->x()) + (rhs.y() - this->y()) * (rhs.y() - this->y()));
     }
 
-    // get the distance if two points are not the same type
-    template<typename U>
-    double distance(const Point<U>& rhs)const
+    // compute and return a new Point rotated with angle radians in anticlockwise direction around the origin
+    // angle: radians
+    Point rotated(double angle)
     {
-        T rhs_x = static_cast<T>(rhs.x());
-        T rhs_y = static_cast<T>(rhs.y()); // conversion
-        Point<T> p_conversion(rhs_x, rhs_y);
-        return distance(p_conversion);
+        return rotated(angle, Point());
+    }
+
+    // compute and return a new Point rotated with angle radians in anticlockwise direction around the certain point
+    // angle: radians
+    Point rotated(double angle, const Point& rhs)
+    {
+        double new_x = (this->x()-rhs.x())*cos(angle) - (this->y()-rhs.y())*sin(angle) + rhs.x();
+        double new_y = (this->x()-rhs.x())*sin(angle) + (this->y()-rhs.y())*cos(angle) + rhs.y();
+        return Point(new_x, new_y);
+    }
+
+    // similar with rotated, return the current instance
+    Point& rotate(double angle)
+    {
+        return rotate(angle, Point());
+    }
+    Point& rotate(double angle, const Point& rhs) 
+    {
+        double new_x = (this->x() - rhs.x()) * cos(angle) - (this->y() - rhs.y()) * sin(angle) + rhs.x();
+        double new_y = (this->x() - rhs.x()) * sin(angle) + (this->y() - rhs.y()) * cos(angle) + rhs.y();
+        this->x() = new_x;
+        this->y() = new_y;
+        return *this;
     }
 
     // print a point, const qualifier: allow const object to access this method
@@ -151,6 +130,6 @@ public:
         return os;
     }
 protected:
-    T _x;
-    T _y;
+    double _x;
+    double _y;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,20 @@
 
 int main()
 {
-	Point<int> p1(2.0, 2.0);
-	Point<double> p2(1.0, 2.0);
+	Point p1(2.0, 2.0);
+	int a = 1, b = 2;
+	Point p2(a, b);
+	Point p = { 1,2 };
 	std::cout << p2.distance() << '\n';
 	std::cout << p2.distance(p1) << '\n';
+
+	Point p_old(2.0, 0.0);
+	Point p_new = p_old.rotated(M_PI / 2);
+	p_new.print();
+
+	Point p_self_old(2.0, 0.0);
+	p_self_old.rotate(M_PI / 2);
+	p_self_old.print();
+
 	return 0;
 }
