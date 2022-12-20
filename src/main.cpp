d59733195658d5924d38eae3b811069c35c7384d
SEUZFY <zmocheng@gmail.com>
2022-12-20 19:06:34 +0100

    Add code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,111 @@
-#include <functional>
 #include <iostream>
+#include <typeinfo>
+#define PRINT_EXPRESSION(expr) std::cout << #expr << ": " << (expr) \
+    << " (type: " << typeid(expr).name() << ")" << std::endl
 
-// Your implementation of the abstract base class goes here
-class Derivative
+template <typename T>
+T add_simple(const T& a, const T& b)
 {
-public: //[DO NOT MODIFY/REMOVE THIS GETTER: IT IS USED IN THE SPECTEST]
-    double GetH() const { return h; }
+    return (a + b);
+}
+
+template <typename T, typename U>
+auto add(const T& a, const U& b) -> decltype(a + b)
+{
+    return (a + b);
+}
+
+template <typename T, typename U>
+auto multiply(const T& a, const U& b) -> decltype(a + b)
+{
+    return (a * b);
+}
+
+template <typename T, typename U>
+auto divide(const T& a, const U& b) -> decltype(a + b)
+{
+    assert(b != static_cast<U>(0));
+    return (a / b);
+}
+
+template <typename T>
+bool is_int(T t) {
+    return false;
+}
+
+template <>
+bool is_int(int i) {
+    return true;
+}
+
+
+
+template <typename T>
+class Number {
 public:
-    Derivative():h(1e-8)
+    const T value; 
+
+    // constructor that takes a value of type T and initializes the attribute
+    Number(const T& value) : value(value) 
     {}
 
-    Derivative(double H):h(H)
-    {}
+    // operators
+    template <typename U>
+    auto operator+(const Number<U>& rhs) const{
+        return Number<decltype(add(value, rhs.value))>(add(value, rhs.value));
+    }
 
-    // pure virtual function
-    virtual double differentiate(const std::function<double(double)>& func, double x) const = 0;
-protected:
-    double h;
-};
+    template <typename U>
+    auto operator-(const Number<U>& rhs) const {
+        return Number<decltype(add(value, -rhs.value))>(add(value, -rhs.value));
+    }
 
+    template <typename U>
+    auto operator*(const Number<U>& rhs) const {
+        return Number<decltype(multiply(value, rhs.value))>(multiply(value, rhs.value));
+    }
 
-// Your implementation of the derived class for the central difference scheme goes here
-class CentralDifference : public Derivative 
-{
-public:
-    using Derivative::Derivative; // inherit the constructors from the base class
-public:
-    virtual double differentiate(const std::function<double(double)>& func, double x) const override final
-    {
-        return ((func(x + h) - func(x - h)) / (2 * h));
+    template <typename U>
+    auto operator/(const Number<U>& rhs) const {
+        return Number<decltype(divide(value, rhs.value))>(divide(value, rhs.value));
     }
 };
 
 
-// Your implementation of the derived class for the forward difference scheme goes here
-class ForwardDifference : public Derivative
-{
-public:
-    using Derivative::Derivative; // inherit the constructors from the base class
-public:
-    virtual double differentiate(const std::function<double(double)>& func, double x) const override final
-    {
-        return ((func(x + h) - func(x)) / h);
-    }
+template <int n>
+struct fibonacci {
+    static const int value = fibonacci<n - 1>::value + fibonacci<n - 2>::value;
 };
 
+// template specialization for the first two Fibonacci numbers
+template <>
+struct fibonacci<0> {
+    static const int value = 0;
+};
 
-double test1(double x) { return x; }
-auto test2 = [](double x) { return x; };
+template <>
+struct fibonacci<1> {
+    static const int value = 1;
+};
 
 
 int main()
 {
-    // Your tests go here
-    CentralDifference central_diff_default;
-    std::cout << central_diff_default.differentiate(test1, 10);
-    std::cout << central_diff_default.differentiate(test2, 10);
+    PRINT_EXPRESSION(add_simple(1, 2));
+    PRINT_EXPRESSION(add_simple(1.5, 2.2));
+    PRINT_EXPRESSION(add(1, 2));
+    PRINT_EXPRESSION(add(1.0, 2));
+    
+    std::cout << (Number<int>(2) * Number<double>(1.2)).value << std::endl;
 
-    ForwardDifference forward_diff_default;
-    std::cout << forward_diff_default.differentiate(test1, 10);
-    std::cout << forward_diff_default.differentiate(test2, 10);
+    // print the first 10 Fibonacci numbers
+    std::cout << fibonacci<0>::value << '\n';
+    std::cout << fibonacci<1>::value << '\n';
+    std::cout << fibonacci<2>::value << '\n';
+    std::cout << fibonacci<3>::value << '\n';
+    std::cout << fibonacci<4>::value << '\n';
+    std::cout << fibonacci<5>::value << '\n';
 
-    CentralDifference central_diff_1(0.1);
-    std::cout << central_diff_1.differentiate(test1, 10);
-    std::cout << central_diff_1.differentiate(test2, 10);
-
-    ForwardDifference forward_diff_1(0.1);
-    std::cout << forward_diff_1.differentiate(test1, 1.0);
-    std::cout << forward_diff_1.differentiate(test2, 1.0);
-
+    std::cout << std::endl;
     return 0;
 }
